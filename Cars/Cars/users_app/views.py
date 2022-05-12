8649ca7b2cfdec684b9d60f5ccd1ff44c6b3314a
Denislav <deni_9000_1990@abv.bg>
2022-05-12 21:29:59 +0300

    added date picker

--- Cars/Cars/users_app/views.py
+++ Cars/Cars/users_app/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth import mixins as auth_mixin
 from django.contrib.auth import forms as auth_forms
 
 
+
 class MyLoginView(LoginView,auth_mixin.LoginRequiredMixin):
     template_name = 'index.html'
     def get_success_url(self):
